77436b0e65767bf641118b14af4a95a89e505900
Rahul <rnkr99@gmail.com>
2021-06-18 21:25:53 +0530

    add cors protection

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000 || 5000 || 5500;
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
 connectDB();
+
+//cors for protection
+
+const corsOptions= {
+    origin: process.env.ALLOWED_CLIENTS.split(',') 
+    //website for cross server like localhost, heroku-git
+
+}
+app.use(cors(corsOptions));
 //template engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
