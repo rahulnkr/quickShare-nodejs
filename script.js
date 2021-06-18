03da9030be32e745cfc29b719cebb87f00fbfdaa
Rahul <rnkr99@gmail.com>
2021-06-18 23:00:42 +0530

    time updated

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-const File = require('./models/file');
-
-const fs = require('fs');
-
 const connectDB = require('./config/db');
+const File = require('./models/file');
+const fs = require('fs');
 
 connectDB();
 
-async function dataCollect() {
-    const pastDate = new Date(Date.now() - 24 *  60 * 60 * 1000);
-    //delete old files in heroku server and mongo db
-    const files = await File.find({ createdAt: { $lt: pastDate } });
-    if(files.length){
-       for(const file of files){
-            try{
+// Get all records older than 24 hours 
+async function fetchData() {
+    const files = await File.find({ createdAt : { $lt: new Date(Date.now() - 60 * 1000)} });
+    if(files.length) {
+        for (const file of files) {
+            try {
                 fs.unlinkSync(file.path);
                 await file.remove();
-                console.log(`successfully deleted.${file.filename}`);
-            }catch(err){
-                console.log(`Error while deleting file ${err}`);
+                console.log(`successfully deleted ${file.filename}`);
+            } catch(err) {
+                console.log(`error while deleting file ${err} `);
             }
-
-       } 
-       console.log('Job Done!');
+        }
     }
+    console.log('Job done!');
 }
 
-dataCollect().then(process.exit);
+fetchData().then(process.exit);
